2e7994d94781709a48b8f62289caa119630da0f2
Timokin233 <50899865+Timolop233@users.noreply.github.com>
2021-07-05 03:00:06 +0800

    Update bili_Captain_Statistics.py

--- bili_Captain_Statistics.py
+++ bili_Captain_Statistics.py
@@ -1,15 +1,10 @@
 import requests
 
-test = False #是否开启测试模式
-
 page = 1 #页数
 
-if test == True:
-    url = f'https://api.live.bilibili.com/xlive/app-room/v1/guardTab/topList?roomid=4895312&page={page}&ruid=1576121'
-elif test == False:
-    room_id = input("请输入直播间房间号：")
-    ruid = input("请输入主播uid：")
-    url = f'https://api.live.bilibili.com/xlive/app-room/v1/guardTab/topList?roomid={room_id}&page={page}&ruid={ruid}'
+room_id = input("请输入直播间房间号：")
+ruid = input("请输入主播uid：")
+url = f'https://api.live.bilibili.com/xlive/app-room/v1/guardTab/topList?roomid={room_id}&page={page}&ruid={ruid}'
 
 headers = {
     'Host': "api.live.bilibili.com",
@@ -26,13 +21,19 @@ print("舰长数为:",num)
 print("正在处理，请稍后.....")
 
 while num:
-    url = f'https://api.live.bilibili.com/xlive/app-room/v1/guardTab/topList?roomid={room_id}&page={page}&ruid={ruid}&1576121&page_size=10'
+    url = f'https://api.live.bilibili.com/xlive/app-room/v1/guardTab/topList?roomid={room_id}&page={page}&ruid={ruid}&1576121page_size=10'
     response = requests.request('get', url, headers=headers).json()
     order = order + 1
     uid = response['data']['list'][list_pages]['uid']
     name = response['data']['list'][list_pages]['username']
-    if test == True:
-        print(order, uid, "", name )
+    fleet_Type = response['data']['list'][list_pages]['guard_level']
+    if fleet_Type == 1:
+        fleet_Test = '总督'
+    elif fleet_Type == 2:
+        fleet_Test = '提督'
+    elif fleet_Type == 3:
+        fleet_Test = '舰长'
+    print(order, uid, "", name, fleet_Test)
     list_pages = list_pages + 1 #读取下一列数据
     if list_pages == 10:
         page = page + 1 #翻页
